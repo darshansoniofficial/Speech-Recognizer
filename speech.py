742283a93e4f7f696204494659a7fad2ca54d9fe
Darshan Soni <101495761+darshansoniofficial@users.noreply.github.com>
2022-04-06 21:42:29 +0530

    Update speech.py

--- speech.py
+++ speech.py
@@ -2,9 +2,13 @@ import speech_recognition as sr
 import pyaudio
 
 r = sr.Recognizer()
+
 with sr.Microphone() as source:
-    print("Sambhdavo ;) ")
-    audio_data = r.record(source, duration=5)
-    print("Tame Bolya...")
-    text = r.recognize_google(audio_data)
-    print(text)
+    try:
+        print("listening..... ")
+        audio_data = r.record(source, duration=10)
+        print("Reorganizing.....")
+        text = r.recognize_google(audio_data)
+        print(text)
+    except:
+        print("Sorry I Can't Understand :D")
